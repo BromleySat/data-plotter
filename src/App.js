fb6cac8b9f74663c7e66703e21660e2bd54c3a65
Kieszku94 <kieszku94@gmail.com>
2021-12-22 11:57:36 +0100

    Chart displaying data from an API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import logo from "./Logo.jpg";
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import {
   LineChart,
   Line,
@@ -10,57 +12,34 @@ import {
   Legend
 } from 'recharts';
 
+
 function App() {
 
+  const [data, setData] = useState([]);
 
-const voltage = (Math.floor(Math.random() * 100 + 1));
-const voltage2 = (Math.floor(Math.random() * 100 + 1));
-const voltage3 = (Math.floor(Math.random() * 100 + 1));
-const voltage4 = (Math.floor(Math.random() * 100 + 1));
-const voltage5 = (Math.floor(Math.random() * 100 + 1));
-const voltage6 = (Math.floor(Math.random() * 100 + 1));
-const voltage7 = (Math.floor(Math.random() * 100 + 1));
-const voltage8 = (Math.floor(Math.random() * 100 + 1));
-const voltage9 = (Math.floor(Math.random() * 100 + 1));
-const voltage10 = (Math.floor(Math.random() * 100 + 1));
-const voltage11 = (Math.floor(Math.random() * 100 + 1));
-const voltage12 = (Math.floor(Math.random() * 100 + 1));
-console.log(voltage)
-
-const data = [
-  {
-    baudRate: 1362,
-    VoltageA: voltage,
-    VoltageB: voltage2,
-  },
-  {
-    baudRate: 1462,
-    VoltageA: voltage3,
-    VoltageB: voltage4,
-  },
-  {
-    baudRate: 1562,
-    VoltageA: voltage5,
-    VoltageB: voltage6,
-  },
-  {
-    baudRate: 1662,
-    VoltageA: voltage7,
-    VoltageB: voltage8,
-  },
-  {
-    baudRate: 1762,
-    VoltageA: voltage9,
-    VoltageB: voltage10,
-  },
-  {
-    baudRate: 1862,
-    VoltageA: voltage11,
-    VoltageB: voltage12
+  const getData = () => {
+    axios.get('/data')
+    .then(res => {
+        
+        setData([...data, res.data]);
+        
+    })
   }
-]
+
+  const [temp, setTemp] = useState(0)
+
+  useEffect(()=>{
+    setInterval(()=>{
+    setTemp((prevTemp)=>prevTemp+1)
+  }, 2000)
+}, [])
+
+  useEffect(()=>{
+  getData()
+}, [temp])
 
 
+console.log(data);
 
  return(
     <div className="App" style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
@@ -68,13 +47,14 @@ const data = [
       <h1 style={{ color: "green"}}>BromleySat's Serial Plotter</h1>
       <LineChart width={1000} height={300} data={data}>
         <CartesianGrid></CartesianGrid>
-        <XAxis dataKey="baudRate"></XAxis>
+        <XAxis dataKey="date"></XAxis>
         <YAxis></YAxis>
         <Tooltip></Tooltip>
         <Legend></Legend>
-        <Line type="monotone" dataKey="VoltageA" stroke="red"></Line>
-        <Line type="monotone" dataKey="VoltageB" stroke="blue"></Line>
+        <Line type="monotone" dataKey="temperatureC" stroke="red"></Line>
+        <Line type="monotone" dataKey="temperatureF" stroke="blue"></Line>
       </LineChart>
+      <button onClick={getData}>Fetch API</button>
     </div>
   )
 }
