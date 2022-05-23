6db4d4d0817ef0d4d7cd3299bf5d7d1580f6a91b
Kieszku94 <kieszku94@gmail.com>
2022-05-23 09:07:05 +0100

    .

---
+++ simulator-node/router.js
@@ -0,0 +1,41 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function makeId(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const deviceId = makeId(23);
+
+const getDate = () => {
+  var today = new Date();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  return time;
+};
+
+const getData = () => {
+  return {
+    deviceId: deviceId,
+    date: getDate(),
+    temperatureC: getRandomIntInclusive(-20, 55),
+    temperatureF: getRandomIntInclusive(-20, 55),
+  };
+};
+
+module.exports = function (app) {
+  app.get("/data", function (req, res) {
+    const data = getData();
+    res.send(data);
+  });
+};
