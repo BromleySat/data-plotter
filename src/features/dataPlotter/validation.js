ccc791f11f019fc5f61908499ad12b979b0a151f
Kieszku94 <kieszku94@gmail.com>
2022-05-25 11:13:43 +0100

    .

--- src/features/dataPlotter/validation.js
+++ src/features/dataPlotter/validation.js
@@ -7,32 +7,50 @@ function isCIDR(ip) {
   return true;
 }
 
+// function multipleEntries(str) {
+//   let newStr;
+//   let strArray = str.split(",");
+//   console.log(strArray);
+//   for (let i = 0; i < strArray.length; i++) {
+//     strArray[i] = strArray[i] + ",";
+//     if (strArray[strArray.length - 1].includes(",")) {
+//       newStr = strArray[strArray.length - 1].replace(",", "");
+//     }
+//     return false;
+//   }
+// }
+
 export function validateInput(input) {
   const specialChars = /[`!@#$%^&*()_+\=\[\]{};'"\\|<>\?~]/;
   const validDomain =
     /^((http|https):\/\/)?([a-zA-Z0-9_][-_a-zA-Z0-9]{0,62}\.)+([a-zA-Z0-9\/]+([a-zA-Z0-9]){1,10})$/g;
-  const multipleEntries = input.split(", ");
   if (specialChars.test(input)) {
     return false;
   }
-  if (validDomain.test(input)) {
-    return true;
-  }
+
   if (input.length > 100) {
     return false;
   }
-  if (input.length > 1 && input.length <= 10) {
+  if (input.length <= 10) {
     return false;
   }
-  if (input.length === 1) {
+
+  if (validDomain.test(input)) {
+    return true;
+  }
+  if (!input.includes(",")) {
     return false;
   }
   if (/\s/.test(input)) {
     return true;
   }
-  if (multipleEntries.length > 1) {
-    return true;
+
+  if (!validDomain.test(input)) {
+    return false;
   }
+  // if (multipleEntries(input)) {
+  //   return true;
+  // }
   // If is a cidr range extract the first part of it which is everything before the slash,
   // If not local Ip return false
   return true;
