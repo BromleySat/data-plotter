21dfd13c0a61242653d4f113a1811aed9a2e66fc
Kieszku94 <kieszku94@gmail.com>
2022-08-12 16:39:28 +0100

    .

---
+++ src/features/dataRetention/dataRetention.spec.js
@@ -0,0 +1,27 @@
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render } from "@testing-library/react";
+import * as React from "react";
+//import { storageSetItem } from "./dataPlotter";
+import DataRetention from "./dataRetention";
+
+describe("Data Retention Functionality", () => {
+  it("should be visible", () => {
+    const { getByTestId } = render(
+      <DataRetention validUrl="https://api.bromleysat.space/api/data" />
+    );
+    const dataRetention = getByTestId(
+      "data-retention-https://api.bromleysat.space/api/data"
+    );
+    expect(dataRetention).toBeVisible();
+  });
+  it("should have the context value of 21d", () => {
+    const { getByTestId } = render(
+      <DataRetention validUrl="https://api.bromleysat.space/api/data" />
+    );
+    const dataRetention = getByTestId(
+      "data-retention-https://api.bromleysat.space/api/data"
+    );
+    expect(dataRetention).toHaveTextContent("21d");
+  });
+  it("should click on the Data Retention icon and check if dropdown menu is visible", () => {});
+});
