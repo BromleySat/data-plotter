a890bd55709b70bf9a1366a96bc64cf0e1e0828e
Kieszku94 <kieszku94@gmail.com>
2022-08-15 09:34:58 +0100

    .

--- src/features/dataRetention/dataRetention.spec.js
+++ src/features/dataRetention/dataRetention.spec.js
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import * as React from "react";
-//import { storageSetItem } from "./dataPlotter";
 import DataRetention from "./dataRetention";
 
 describe("Data Retention Functionality", () => {
@@ -14,7 +13,7 @@ describe("Data Retention Functionality", () => {
     );
     expect(dataRetention).toBeVisible();
   });
-  it("should have the context value of 21d", () => {
+  it("should have the text content value of 21d", () => {
     const { getByTestId } = render(
       <DataRetention validUrl="https://api.bromleysat.space/api/data" />
     );
@@ -23,5 +22,17 @@ describe("Data Retention Functionality", () => {
     );
     expect(dataRetention).toHaveTextContent("21d");
   });
-  it("should click on the Data Retention icon and check if dropdown menu is visible", () => {});
+  it("should click on the Data Retention icon and check if dropdown menu is visible", () => {
+    const { getByTestId } = render(
+      <DataRetention validUrl="https://api.bromleysat.space/api/data" />
+    );
+    const dataRetention = getByTestId(
+      "data-retention-https://api.bromleysat.space/api/data"
+    );
+    fireEvent.click(dataRetention);
+    screen.debug();
+    expect(
+      getByTestId("data-retention-10s-https://api.bromleysat.space/api/data")
+    ).toBeInTheDocument();
+  });
 });
