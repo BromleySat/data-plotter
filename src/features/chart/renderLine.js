d300f2cdfbd0934c67a49247a38ee60f9c3b2026
Kieszku94 <kieszku94@gmail.com>
2023-01-19 11:04:57 +0000

    .

--- src/features/chart/renderLine.js
+++ src/features/chart/renderLine.js
@@ -17,33 +17,28 @@ export const RenderLine = (data) => {
         column !== "currentTime" &&
         column !== "number"
     );
-  console.log(columns);
-  let numArray = [];
   let values = Object.entries(data[data.length - 1])
     .map(([key, value]) => {
-      if (key === "time" || key === "currentTime" || key === "date") {
+      if (
+        key === "deviceId" ||
+        key === "time" ||
+        key === "date" ||
+        key === "currentTime" ||
+        key === "number"
+      ) {
         return false;
       }
       return value;
     })
     .filter((value) => value !== false);
 
-  console.log(values);
-
-  for (let value of values) {
-    if (Number.isInteger(value)) {
-      numArray.push(value);
-    }
-  }
-
   if (columns.length > 5) {
     columns = columns.slice(0, 5);
-    values = values.slice(0, 5);
   }
   let i = 0;
   return columns.map((column, index) => {
-    const indexValue = numArray[index];
-    if (indexValue < 5) {
+    const indexValue = values[index];
+    if (indexValue < 5 && column === "TemperatureF") {
       return (
         <Line
           yAxisId="right-axis"
