950851ab4cdf4a22ed028eedf9d56ca9ab8c907e
Kieszku94 <kieszku94@gmail.com>
2023-01-20 13:16:59 +0000

    .

--- simulator-typescript/data/data.ts
+++ simulator-typescript/data/data.ts
@@ -36,7 +36,7 @@ export function getData(): Data {
   return {
     date: getDate(),
     temperatureC: getRandomIntInclusive(-60, 60),
-    temperatureF: getRandomIntInclusive(0, 3),
+    temperatureF: getRandomIntInclusive(-75, 3),
     voltage: getRandomIntInclusive(0, 240),
     waterLevel: getRandomIntInclusive(0, 150),
   };
