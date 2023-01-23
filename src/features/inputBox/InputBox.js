6afad49977a709770bc50930779da5a5ce44f70a
Kieszku94 <kieszku94@gmail.com>
2023-01-23 10:34:08 +0000

    .

--- src/features/inputBox/InputBox.js
+++ src/features/inputBox/InputBox.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useRef, useCallback } from "react";
 import "./inputBox.css";
 import { makeStyles } from "@material-ui/core/styles";
 import { useTheme } from "@material-ui/core/styles";
+import { trimHttp } from "../helpers/trimHttp";
+import { storageSetItem } from "../helpers/storageSetItem";
+import { getApiList } from "./validation";
+import { isLocalIp } from "./validation";
 
 const InputBox = () => {
   const theme = useTheme();
+  const intervalRef = useRef(null);
+  const publicApi = "https://api.bromleysat.space";
+  const [urlList, setUrlList] = useState(
+    JSON.parse(localStorage.getItem("urlList")) || [publicApi]
+  );
+  const [textBoxValue, setTextBoxValue] = useState(trimHttp(urlList).join(","));
+  const [error, setError] = useState(false);
+  const [validUrls, setValidUrls] = useState([]);
+  const [devicesId, setDevicesId] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [running, setRunning] = useState([]);
+  const getDataRef = useRef();
+
   const useStyles = makeStyles({
     root: {
       "&::before": {
@@ -33,6 +50,18 @@ const InputBox = () => {
   });
 
   const classes = useStyles();
+
+  const noApiConfigStored = useCallback(() => {
+    const localIp = isLocalIp(window.location.host);
+    if (localIp) {
+      storageSetItem(
+        "urlList",
+        JSON.stringify(getApiList(window.location.host))
+      );
+      console.log(localStorage.getItem("urlList"));
+      setUrlList(getApiList(window.location.host));
+    }
+  }, [setUrlList]);
   return <div>InputBox</div>;
 };
 
