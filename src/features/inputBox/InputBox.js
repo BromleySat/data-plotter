7ff26a5fdb66071697aa8c63e6e9fc55eb80c537
Kieszku94 <kieszku94@gmail.com>
2023-01-23 10:25:05 +0000

    .

--- src/features/inputBox/InputBox.js
+++ src/features/inputBox/InputBox.js
@@ -1,7 +1,38 @@
 import React from "react";
 import "./inputBox.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 
 const InputBox = () => {
+  const theme = useTheme();
+  const useStyles = makeStyles({
+    root: {
+      "&::before": {
+        borderBottom: `1px solid ${theme.palette.text.primary}`,
+        display: "none",
+      },
+      "&::after": {
+        borderBottom: `1px solid ${theme.palette.text.primary}`,
+        display: "none",
+      },
+      "& .MuiInput-input": {
+        minWidth: "350px",
+        fontFamily: "Quicksand",
+        fontWeight: "700",
+        color: `${theme.palette.text.primary}`,
+        borderBottom: `1px solid ${theme.palette.text.primary}`,
+      },
+      "@media (max-width: 30em)": {
+        "& .MuiInput-input": {
+          minWidth: "275px",
+        },
+      },
+    },
+    disabled: {},
+    notchedOutline: {},
+  });
+
+  const classes = useStyles();
   return <div>InputBox</div>;
 };
 
