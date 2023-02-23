eae2ec5a302e271efdd33ad70e3d3e497c368063
Kieszku94 <kieszku94@gmail.com>
2023-02-23 10:23:13 +0000

    .

--- src/components/DataRetention/DataRetention.js
+++ src/components/DataRetention/DataRetention.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { storageSetItem } from "../../helpers/storageSetItem";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -6,14 +7,27 @@ import InputLabel from "@mui/material/InputLabel";
 import { useTheme } from "@material-ui/core/styles";
 import AutoDeleteSharpIcon from "@mui/icons-material/AutoDeleteSharp";
 import ControlledTooltip from "../Tooltip/Tooltip";
+import {
+  useDataRetention,
+  useSetDataRetention,
+} from "../../context/chartContext/chartControlContext";
 
 const DataRetention = ({ validUrl }) => {
+  const dataRetention = useDataRetention();
+  const setDataRetention = useSetDataRetention();
   const [tooltipOpen, setTooltipOpen] = useState(false);
   const theme = useTheme();
 
   const handleTooltip = (bool) => {
     setTooltipOpen(bool);
   };
+
+  useEffect(() => {
+    if (localStorage.getItem(`DATA RETENTION FOR ${validUrl}`) !== null) {
+      setDataRetention(localStorage.getItem(`DATA RETENTION FOR ${validUrl}`));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ControlledTooltip
@@ -44,11 +58,11 @@ const DataRetention = ({ validUrl }) => {
           labelId="demo-select-small"
           data-testid={`data-retention-${validUrl}`}
           id="demo-select-small"
-          value={
-            localStorage.getItem(`DATA RETENTION FOR ${validUrl}`)
-              ? localStorage.getItem(`DATA RETENTION FOR ${validUrl}`)
-              : 1814400000
-          }
+          value={dataRetention}
+          onChange={(e) => {
+            storageSetItem(`DATA RETENTION FOR ${validUrl}`, e.target.value);
+            setDataRetention(e.target.value);
+          }}
           IconComponent={AutoDeleteSharpIcon}
           sx={{
             color: theme.palette.text.primary,
