6750b0ed0634ad47c74e53912102bd69d4c1a803
Kieszku94 <kieszku94@gmail.com>
2023-02-23 10:59:43 +0000

    .

--- src/helpers/dataRetention/dataRetention.js
+++ src/helpers/dataRetention/dataRetention.js
@@ -1,6 +1,6 @@
-export const dataRetention = (data, dataRetention) => {
+export const dataRetention = (data, dataRetention, currentTime) => {
   for (let i = data.length - 1; i >= 0; i--) {
-    if (data[i].currentTime < dataRetention) return data.slice(i);
+    if (data[i].currentTime < currentTime - dataRetention) return data.slice(i);
   }
   return data;
 };
