072fbf67c4ca1e315a30a03df7d69e0ed126c588
Kieszku94 <kieszku94@gmail.com>
2023-02-24 12:20:14 +0000

    .

--- src/helpers/dataRetention/dataRetention.js
+++ src/helpers/dataRetention/dataRetention.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 export const dataRetention = (data, dataRetention, time) => {
   const lastIndex = _.findLastIndex(
     data,
-    (el) => el.time >= time - dataRetention
+    (el) => el.time <= time - dataRetention
   );
   if (lastIndex !== -1) return data.slice(lastIndex + 1);
   return data;
