06d14ceaf07ac726d0051e21e80475481ee415b2
Kieszku94 <kieszku94@gmail.com>
2023-02-24 15:40:47 +0000

    .

--- src/helpers/chartTimeWindow/chartTimeWindow.js
+++ src/helpers/chartTimeWindow/chartTimeWindow.js
@@ -1,5 +1,5 @@
 export const chartTimeWindow = (data, chartTimeWindow, time) => {
   const filteredData = data.filter((el) => el.time >= time - chartTimeWindow);
   if (filteredData.length > 0) return filteredData;
-  return data;
+  return [];
 };
